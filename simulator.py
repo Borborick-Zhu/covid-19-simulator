44b76db07dcd02f46a1d95f029bfd3a73f0b7508
Borborick <borborickzhu@g.ucla.edu>
2022-08-11 13:19:42 +1000

    covid-19 simulator start

---
+++ simulator.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import numpy as np
+
+GREY = (0.78, 0.78, 0.78)
+RED = (0.96, 0.15, 0.15)
+GREEN = (0, 0.86, 0.03)
+BLACK = (0, 0, 0)
