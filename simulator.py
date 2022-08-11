3edc5a1db4168e1e02b5ee85efe6696c688a4f6d
Borborick <borborickzhu@g.ucla.edu>
2022-08-11 14:08:17 +1000

    initial changes - nothing much

--- simulator.py
+++ simulator.py
@@ -6,3 +6,11 @@ GREY = (0.78, 0.78, 0.78)
 RED = (0.96, 0.15, 0.15)
 GREEN = (0, 0.86, 0.03)
 BLACK = (0, 0, 0)
+
+class Person:
+  def __init_(self, x, y, infected, recovered):
+    self.x = x
+    self.y = y
+    self.infected = False
+    self.recovered = False
+
