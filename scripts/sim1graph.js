6f363c08be2a0912cfb5198a38431b7675e014b3
Borborick Zhu <borborickzhu@gmail.com>
2022-09-08 14:48:34 +1000

    white background

--- scripts/sim1graph.js
+++ scripts/sim1graph.js
@@ -3,14 +3,14 @@ var sim1graph = function(p) {
     
     p.setup = function() {
         p.createCanvas(1100, 400);
-        p.background(0);
+        p.background(255);
         p.frameRate(FRAME_RATE);
     };
 
     function createLineDrawer(numberOfBalls) {
         
         var lineDrawer = new Object();
-        
+
     }
 
 }
