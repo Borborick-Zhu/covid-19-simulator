3a3dd033bfe5fd47838b125e8f2e63859fbdf76a
Borborick Zhu <borborickzhu@gmail.com>
2022-09-08 15:17:24 +1000

    comments

--- scripts/sim1graph.js
+++ scripts/sim1graph.js
@@ -2,14 +2,23 @@ var sim1graph = function(p) {
     const FRAME_RATE = 10;
     
     p.setup = function() {
-        p.createCanvas(1100, 400);
+        p.createCanvas(1100, 500);
         p.background(255);
         p.frameRate(FRAME_RATE);
     };
 
+    p.draw = function() {
+        lineDrawer.draw();
+    }
+
     function createLineDrawer(numberOfBalls) {
         
-        var lineDrawer = new Object();
+        // skeches a bunch of dots.
+        //essentially create a class. 
+        // its y value will be its respective value and its x intercept
+        // will be its current frame. 
+        // stop plotting when it has reached the edge (1100 px)
+
 
     }
 
